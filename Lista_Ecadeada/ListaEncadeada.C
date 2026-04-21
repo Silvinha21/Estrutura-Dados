4e9453d229c0646abca10ca01b9249da16b649d5
Arthur <arthursilvasoares60@gmail.com>
2026-04-21 17:28:16 -0300

    Inserindo função de remover na lista encadeada, e menu de opções

--- Lista_Ecadeada/ListaEncadeada.C
+++ Lista_Ecadeada/ListaEncadeada.C
@@ -12,7 +12,7 @@ struct Celula *nova, *aux, *aux2;
 nova = (struct Celula *)malloc(sizeof(struct Celula));
 
 if(!nova){
-printf("Nao tem memoria disponivel!");
+    printf("Nao tem memoria disponivel!");
 
 return cabeca;
 }
@@ -33,19 +33,103 @@ if(!cabeca){
         nova->prox = aux;
         return nova;
     } else {
+        // se pá que esse bagui tem algum buraco que eu não vi, mas como não consigo executar vai ficar assim mesmo
         while (aux->prox && aux->prox->valor < nova->valor){
             aux = aux->prox;
         }
-        nova->prox = aux->prox;
-        aux->prox = nova;
+        if(!aux->prox){
+            nova->prox = 0;
+            aux->prox = nova;
+        } else {
+            nova->prox = aux->prox;
+            aux->prox = nova;
+        }
+    }
+}
+return cabeca;
+}
+
+
+struct Celula * remover(struct Celula *cabeca){
+    struct Celula *aux;
+    int valor;
+
+    printf("Qual valor deseja remover? ");
+    scanf("%d", &valor);
+
+    if(!cabeca){
+        printf("Lista vazia, nada para remover!");
+        return cabeca;
+    } else if (cabeca->valor == valor)
+    {
+        cabeca = cabeca->prox;
+        return cabeca;
+    }
+    
+    aux = cabeca;
+    while(aux->prox && aux->prox->valor != valor){
+        aux = aux->prox;
+    } 
+    if(!aux->prox){
+        printf("Valor nao encontrado!");
+        return cabeca;
+    } else {
+        aux->prox = aux->prox->prox;
+        return cabeca;
+    }
+    
+}
+
+
+
+void imprimir(struct Celula *cabeca){
+    struct Celula *aux;
+
+    if(!cabeca){
+        printf("Lista vazia!");
+    } else {
+        aux = cabeca;
+        while(aux){
+            printf("%d ", aux->valor);
+            aux = aux->prox;
+        }
     }
 }
 
-return cabeca;
-
-
-}
 
 int main(){
+    struct Celula *cabeca = 0;
+    int opcao;
 
+    do{
+        printf("\n1 - Inserir");
+        printf("\n2 - Remover");
+        printf("\n3 - Imprimir");
+        printf("\n4 - Sair");
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            cabeca = inserir(cabeca);
+            break;
+        
+        case 2:
+            cabeca = remover(cabeca);
+            break;
+
+        case 3:
+            imprimir(cabeca);
+            break;
+
+        case 4:
+            printf("Saindo...");
+            break;
+
+        default:
+            printf("Opcao invalida!");
+            break;
+        }
+    } while (opcao != 4);
 }
